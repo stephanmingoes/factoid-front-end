9419665d5733f9bb059673bcd46922eaa814ed98
Stephan Mingoes <stephsvpreme@gmail.com>
2021-07-10 06:27:58 -0500

    changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // "https://factoid-backend.herokuapp.com"
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://factoid-backend.herokuapp.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
